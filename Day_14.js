fe470d5321905f112e0c6e0718c6090d4d852bd8
Prasad2608 <prasadlap2002@gmail.com>
2023-12-06 11:11:16 +0530

    html

--- Day_14.js
+++ Day_14.js
@@ -1,6 +1,7 @@
 var c = 0;
 var image = ["img1.jpg" , "img2.jpg"];
+
 function changeImg(){
-    c = c+1;
-    document.getElementById("#imgChange").innerHTML = 
+    c = (c + 1) % image.length;
+    document.getElementById("imgChange").innerHTML = "<img src='" + image[c] + "'>";
 }
